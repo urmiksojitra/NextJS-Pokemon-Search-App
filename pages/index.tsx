424cf0296d6fccc05058679ea7fa937a74d9b793
Gopal Rathod <gopal@360technosoft.com>
2025-01-08 16:27:43 +0530

    inital commit

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react';
+import { useFetchPokemonWithInfinityScroll } from '../hooks/usePokemon';
+import HomePage from '@/components/pages/HomePage';
+import MyBeatLoader from '@/components/UI/molecules/BeatLoader';
+import useFetchNextPage from '../hooks/useFetchNextPage';
+import Container from '@/components/UI/atoms/Container';
+
+const IndexPage: FC = (): JSX.Element => {
+  const { data, isSuccess, hasNextPage, fetchNextPage, isInitialLoading } =
+    useFetchPokemonWithInfinityScroll();
+
+  useFetchNextPage(hasNextPage, fetchNextPage);
+
+  if (isInitialLoading) {
+    return (
+      <Container page="spinner">
+        <MyBeatLoader loading={isInitialLoading} />
+      </Container>
+    );
+  }
+
+  if (isSuccess) {
+    return <HomePage pokemonList={data} />;
+  }
+
+  return <></>;
+};
+
+export default IndexPage;
